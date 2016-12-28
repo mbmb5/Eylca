59196af6712744bd138f46e18372a96d3c99037d
mbmb5 <monsieur.bmb5@gmail.com>
2016-12-28 16:30:14 +0100

    Remove unused method

--- app/src/main/java/mbmb5/extendedcontrolapp/SettingsActivity.java
+++ app/src/main/java/mbmb5/extendedcontrolapp/SettingsActivity.java
@@ -51,15 +51,6 @@ public class SettingsActivity extends Activity {
     };
 
     /**
-     * Helper method to determine if the device has an extra-large screen. For
-     * example, 10" tablets are extra-large.
-     */
-    private static boolean isXLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-    }
-
-    /**
      * Binds a preference's summary to its value. More specifically, when the
      * preference's value is changed, its summary (line of text below the
      * preference title) is updated to reflect the value. The summary is also
